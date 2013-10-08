b7928c1a0cef3019efae7cdda79d5bcbe8d67662
Karan <karan4c6@gmail.com>
2013-10-08 16:32:30 +0530

    preprocessor

---
+++ preprocessor.c
@@ -0,0 +1,17 @@
+/**
+   * @author    Maven Scientists (http://mavenscientists.com) 
+   * @email    info@mavenscientists.com
+   */
+#include <stdio.h>
+
+#define MAX 1000 //Macro defined
+
+int main()
+{
+	
+	char array[MAX];
+
+	printf("\n%d", MAX);
+	
+	return 0;
+}
